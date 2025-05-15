ac71438ad47ac4160d69e22cb34f568884669f25
sylamarie <sylamariecumuyog@outlook.com>
2025-05-16 03:03:00 +0800

    CHANGES

---
+++ database/assignment2.sql
@@ -0,0 +1,11 @@
+INSERT INTO account (
+  account_firstname,
+  account_lastname,
+  account_email,
+  account_password
+) VALUES (
+  'Tony',
+  'Stark',
+  'tony@starkent.com',
+  'Iam1ronM@n'
+);
