f1cfe4aa8a06f58935c1888d249c98e1c22833bb
sylamarie <sylamariecumuyog@outlook.com>
2025-06-06 08:28:36 +0800

    Server-Side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -9,7 +10,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // GET /account/register
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Process registration data
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
